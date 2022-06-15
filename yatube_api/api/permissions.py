4b9c652a6e6e848a985e500cd78ef621f2188f59
Александр Иванченко <xKapellMeisterx@yandex.ru>
2022-06-15 13:10:17 +0300

    "Второе ревью"

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,6 +3,4 @@ from rest_framework import permissions
 
 class IsOwner(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
         return obj.author == request.user
